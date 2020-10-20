504b0896272d4746a812e51d6fd0c3140429ba1e
Kirill Plyushchai <kirill.plyushchai@gmail.com>
2020-10-20 03:52:04 +0300

    add material

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HeroesComponent} from './heroes/heroes.component';
@@ -12,6 +11,8 @@ import {HttpClientModule} from '@angular/common/http';
 import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 import {InMemoryDataService} from './in-memory-data.service';
 import {HeroSearchComponent} from './hero-search/hero-search.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './material/material.module';
 
 @NgModule({
     imports: [
@@ -19,10 +20,11 @@ import {HeroSearchComponent} from './hero-search/hero-search.component';
         FormsModule,
         AppRoutingModule,
         HttpClientModule,
-        HttpClientInMemoryWebApiModule.forRoot(
-          InMemoryDataService, { dataEncapsulation: false }
-        )
-      ],    providers: [],
+        HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {dataEncapsulation: false}),
+        BrowserAnimationsModule,
+        MaterialModule
+    ],
+    providers: [],
     declarations: [
         AppComponent,
         DashboardComponent,
