b8a0f905dc7f30d848762559a9114ef784bf546f
Kirill Plyushchai <kirill.plyushchai@gmail.com>
2020-11-03 03:10:48 +0300

    sort

--- authApp/src/app/main-page/main-page.component.ts
+++ authApp/src/app/main-page/main-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatTable, MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { USERS } from '../auth/databaseUsers';
 import { AuthService } from '../auth/auth.service';
+import { MatSort } from '@angular/material/sort';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogComponent } from '../dialog/dialog.component';
 
@@ -19,9 +20,15 @@ export class MainPageComponent implements OnInit {
         'id',
         'username',
         'email',
-        'password',
-        'verify',
+        'age',
+        'department',
+        'teamlead',
+        'verification',
         'edit',
+        //age { < 30 }
+        //department { all, all depart, no depart }
+        //teamlead(bool) { teamlead , not teamlead} 
+        //sort ++
     ];
 
     @ViewChild('table') table: MatTable<Element>;
@@ -29,7 +36,6 @@ export class MainPageComponent implements OnInit {
     isLogin() {
         return !this.authService.isLogged;
     }
-
     openDialog(action, obj) {
         obj.action = action;
         const dialogRef = this.dialog.open(DialogComponent, {
@@ -47,16 +53,19 @@ export class MainPageComponent implements OnInit {
             }
         });
     }
-
     createUser(row_obj) {
-        let id = this.userData.data.length;
+        const id = this.userData.data.length;
         this.userData.data.push({
             id: id + 1,
             username: row_obj.username,
             email: row_obj.email,
             password: row_obj.password,
-            verification: false,
+            age: row_obj.age,
+            department: row_obj.department,
+            teamlead: row_obj.department,
+            verification: row_obj.verification,
         });
+        console.log(row_obj);
         this.table.renderRows();
     }
     editUser(row_obj) {
@@ -65,6 +74,10 @@ export class MainPageComponent implements OnInit {
                 value.username = row_obj.username;
                 value.email = row_obj.email;
                 value.password = row_obj.password;
+                value.age = row_obj.age;
+                value.department = row_obj.department;
+                value.teamlead = row_obj.teamlead;
+                value.verification = row_obj.verification;
             }
             return true;
         });
@@ -75,9 +88,52 @@ export class MainPageComponent implements OnInit {
         });
     }
 
+    @ViewChild(MatSort) sort: MatSort;
+
+    ngAfterViewInit() {
+        this.userData.sort = this.sort;
+    }
+
     applyFilter(event: Event) {
         const filterValue = (event.target as HTMLInputElement).value;
         this.userData.filter = filterValue.trim().toLowerCase();
+    }
+
+    // selectedOption: string;
+    // printedOption: string;
+
+    ageParam = [
+        { name: 'all', value: 1 },
+        { name: 'up to 30 years', value: 2 },
+        { name: 'over 30 years', value: 3 },
+    ];
+
+    departmentParam = [
+        { name: 'all', value: 1 },
+        { name: 'all department', value: 2 },
+        { name: 'no department', value: 3 },
+    ];
+
+    teamleadParam = [
+        { name: 'all', value: 1 },
+        { name: 'teamLead', value: 2 },
+        { name: 'not teamlead', value: 3 },
+    ];
+
+    ageFilter() {
+        console.log(this.ageParam);
+        console.log(1);
+        // if (this.selected == '1') {
+        //     this.age = 'yes';
+        // } else if (this.selected == '2') {
+        //     this.age = 'yess';
+        //     // if (age < 30) {
+        //     // this.userData.filter = age.trim().toLowerCase();
+        //     // }
+        // } else if (this.selected == '3') {
+        //     this.age = 'yess';
+        //     // this.userData.filter = age.trim().toLowerCase();
+        // }
     }
 
     ngOnInit(): void {}
