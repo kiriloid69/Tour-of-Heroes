6c9e80b2e3b83b1dc3d7cf77407b3172808d295c
Kirill Plyushchai <kirill.plyushchai@gmail.com>
2020-11-04 02:32:40 +0300

    sort3

--- authApp/src/app/main-page/main-page.component.ts
+++ authApp/src/app/main-page/main-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatTable, MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { USERS } from '../auth/databaseUsers';
 import { AuthService } from '../auth/auth.service';
+import { MatSort } from '@angular/material/sort';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogComponent } from '../dialog/dialog.component';
 
@@ -22,7 +23,6 @@ export class MainPageComponent implements OnInit {
         'id',
         'username',
         'email',
-<<<<<<< HEAD
         'age',
         'department',
         'teamlead',
@@ -32,17 +32,11 @@ export class MainPageComponent implements OnInit {
         //department { all, all depart, no depart }
         //teamlead(bool) { teamlead , not teamlead} 
         //sort ++
-=======
-        'password',
-        'verify',
-        'edit',
->>>>>>> parent of b8a0f90... sort
     ];
 
     isLogin() {
         return !this.authService.isLogged;
     }
-
     openDialog(action, obj) {
         obj.action = action;
         const dialogRef = this.dialog.open(DialogComponent, {
@@ -60,20 +54,19 @@ export class MainPageComponent implements OnInit {
             }
         });
     }
-
     createUser(row_obj) {
-        let id = this.userData.data.length;
+        const id = this.userData.data.length;
         this.userData.data.push({
             id: id + 1,
             username: row_obj.username,
             email: row_obj.email,
             password: row_obj.password,
-            verification: false,
+            age: row_obj.age,
+            department: row_obj.department,
+            teamlead: row_obj.department,
+            verification: row_obj.verification,
         });
-<<<<<<< HEAD
         console.log('я работаюы');
-=======
->>>>>>> parent of b8a0f90... sort
         this.table.renderRows();
         // return true;
     }
@@ -83,6 +76,10 @@ export class MainPageComponent implements OnInit {
                 value.username = row_obj.username;
                 value.email = row_obj.email;
                 value.password = row_obj.password;
+                value.age = row_obj.age;
+                value.department = row_obj.department;
+                value.teamlead = row_obj.teamlead;
+                value.verification = row_obj.verification;
             }
             return true;
         });
@@ -93,12 +90,17 @@ export class MainPageComponent implements OnInit {
         });
     }
 
+    @ViewChild(MatSort) sort: MatSort;
+
+    ngAfterViewInit() {
+        this.userData.sort = this.sort;
+    }
+
     applyFilter(event: Event) {
         const filterValue = (event.target as HTMLInputElement).value;
         this.userData.filter = filterValue.trim().toLowerCase();
     }
 
-<<<<<<< HEAD
     ageOption: string;
     departmentOption: string;
     teamleadOption: string;
@@ -184,7 +186,4 @@ export class MainPageComponent implements OnInit {
     }
 
     ngOnInit(): void { }
-=======
-    ngOnInit(): void {}
->>>>>>> parent of b8a0f90... sort
 }
